0dcd194ab9aa15414d84f04d26935fcb571c3147
a.nvlkv <2813794+anvlkv@users.noreply.github.com>
2025-09-28 19:26:06 +0300

    Update page.rs

--- src/components/page.rs
+++ src/components/page.rs
@@ -303,7 +303,7 @@ pub fn Page(
         <div class="w-full h-full flex items-center justify-center">
             <Card
                 start_animation=Signal::derive(start_animation)
-                class="max-h-screen overflow-auto"
+                class="max-h-screen"
                 on_animation_done=Callback::new({
                     move |(tx_id, kind)| {
                         match kind {

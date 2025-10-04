06d457c0e4a05e9c37c61b80a21205f06c2db228
a.nvlkv <2813794+anvlkv@users.noreply.github.com>
2025-10-04 06:49:45 +0300

    Update ci_post_clone.sh

--- src-tauri/gen/apple/ci_scripts/ci_post_clone.sh
+++ src-tauri/gen/apple/ci_scripts/ci_post_clone.sh
@@ -1,6 +1,8 @@
 # install rust and cargo
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --profile minimal --default-toolchain nightly -y
 
+. "$HOME/.cargo/env"
+
 
 # install cargo-binstall
 curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash

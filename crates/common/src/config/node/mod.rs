4d5c7b66bec7ac53c59418804e7e4f8139434967
a.nvlkv <2813794+anvlkv@users.noreply.github.com>
2026-05-12 22:45:04 +0300

    scoped node methods

--- crates/common/src/config/node/mod.rs
+++ crates/common/src/config/node/mod.rs
@@ -60,6 +60,43 @@ impl Node {
         todo!()
     }
 
+    /// Computes the profile point of the node at a given normalized length `l` (-1 to 1), representing surface of the node's profile.
+    ///
+    /// - -1..0 corresponds to the inner profile
+    /// - 0 is the tip of the rim
+    /// - 0..=1 corresponds to the outer profile
+    /// - the points at -1 and 1 are exactly the same and represent the start of profile drawing, which is the Point2(0,0).
+    ///
+    /// ## Drawing order (-1 to 1):
+    ///
+    /// 1. Start at the origin of coordinate system Point2(0,0)
+    /// 2. Draw line. 1/2 of the crown thickness downwards
+    /// 3. Draw crown segment with 1/2 of the crown thickness applied to baseline normal
+    /// 4. Draw smooth transition from crown to upper body thickness
+    /// 5. Draw upper body segment with 1/2 upper body thickness applied to baseline normal
+    /// 6. Draw smooth transition from upper body to mid-body thickness
+    /// 7. Draw mid-body segment with 1/2 mid-body thickness applied to baseline normal
+    /// 8. Draw smooth transition from mid-body to shoulder thickness
+    /// 9. Draw shoulder segment with 1/2 shoulder thickness applied to baseline normal
+    ///
+    /// 10. Does the inner soundbow segment exist?
+    /// 10.1. Yes - exists.
+    /// 10.1.1. Prepare projection of a rim without bow (see 10.2)
+    /// 10.1.2. Use the bow segment samples to compute a normal offset for the rim projection, creating the soundbow feature as a localized deviation from the baseline rim profile.
+    /// 10.2. No - does not exist.
+    /// 10.2.1. Draw smooth transition from shoulder to rim thickness
+    /// 10.2.2 Draw rim segment with 1/2 rim thickness applied as a normal and reducing the applied thickness to 0 at the tip of the rim (u=0).
+    ///
+    /// 11. Does the outer soundbow segment exist?
+    /// 11.1. Yes - exists. -  Repeat the same process as 10.1 but applied to the outer profile, creating a convex soundbow feature.
+    /// 11.2. No - does not exist. - Draw the outer profile as described in 10.2, without any soundbow deviation.
+    ///
+    /// 12. Repeat steps 9..=2 adjusted for the outer profile.
+    /// 13. Verify the final point at l=1 is the same as the starting point at l=-1, ensuring a closed profile loop.
+    fn profile_at(&self, l: f64) -> Point2<f64> {
+        todo!()
+    }
+
     /// Samples the outer and inner profiles of the node at a given normalized length `u` (0 to 1).
     pub fn sample_at_u(&self, u: f64) -> (Point2<f64>, Point2<f64>) {
         todo!()
